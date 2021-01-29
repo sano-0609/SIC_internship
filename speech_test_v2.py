ea9a93ddbcac9084644ccb1656d563a7e11a5a98
sano-0609 <s193142@stn.nagaokaut.ac.jp>
2021-01-29 15:15:10 +0900

    修正

---
+++ speech_test_v2.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+import time
+
+r = sr.Recognizer()
+start = time.time()
+
+with sr.AudioFile("sample2.wav") as source:
+    audio = r.record(source)
+text = r.recognize_google(audio, language='ja-JP')
+#print(t)
+print(text)
+end_time = time.time() - start
+print("end_time:{0}".format(end_time) + "[sec]")
